23a2079f22f1e66aae9502d6d8b7df76e169808b
jeong-jaejun <vovo4963@easymedia.net>
2024-04-29 17:05:54 +0900

    4.2

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,20 +2,14 @@
 
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {useState} from "react";
-
+import styles from "../styles/Navigation.module.css";
 export default function Navigation() {
     const path = usePathname()
-
-    const [count, setCount] = useState(0)
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
                 <li><Link href="/">Home {path === "/" ? "🏠" : null}</Link></li>
                 <li><Link href="/about-us">About Us {path === "/about-us" ? "📖" : null}</Link></li>
-                <li><Link href="/page">Page</Link></li>
-                <li><Link href="/not-found">Not Found</Link></li>
-                <li><button onClick={()=> setCount((c)=> c + 1)}> {count}</button></li>
             </ul>
         </nav>
     )

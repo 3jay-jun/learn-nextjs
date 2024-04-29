ff8587d6344a25b78c60737bd727e88e12c117af
jeong-jaejun <vovo4963@easymedia.net>
2024-04-29 17:54:55 +0900

    vercel

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,5 +1,5 @@
-import {API_URL} from "../app/(home)/page";
 import styles from './movie-info.module.css'
+import {API_URL} from "../constant";
 
 export const metadata = {
     title: 'Movie Info',
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,5 +1,5 @@
-import {API_URL} from "../app/(home)/page";
 import styles from './movie-videos.module.css'
+import {API_URL} from "../constant";
 
 async function getVideos(id: string) {
     console.log(`Fetching : ${Date.now()}`)
